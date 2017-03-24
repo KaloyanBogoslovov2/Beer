0f2b6d37ad91a8fef4bc29a663cdfb4561491c39
KaloyanBogoslovov2 <kaloyan_bogoslovov@yahoo.com>
2017-03-24 15:02:38 +0200

    minor changes

---
+++ app/src/main/java/com/bogoslovov/kaloyan/beer/Constants.java
@@ -0,0 +1,11 @@
+package com.bogoslovov.kaloyan.beer;
+
+/**
+ * Created by kaloqn on 3/24/17.
+ */
+
+public class Constants {
+    public static final String BASE_URL ="http://api.brewerydb.com/v2/";
+    public static final String BREWERY_DB_KEY = "?key=cf0395fa7bf926b0600e7aa116117a83";
+    public static final String FORMAT = "&format=json";
+}
